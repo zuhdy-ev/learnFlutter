582a96abc534702df846930a3a8be1b4b6fd335d
zuhdy-ev <ardhiekov@gmail.com>
2023-12-06 11:28:27 +0700

    Colours & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: const Text("My First App"),
+      title: const Text(
+        "My First App",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontFamily: "Poppins-SemiBold",
+        ),
+      ),
       centerTitle: true,
+      backgroundColor: Colors.green[800],
+
     ),
     body: const Center(
-      child: Text("Hello World"),
+      child: Text(
+        "Hello World",
+        style: TextStyle(
+          fontSize: 30.0,
+          fontFamily: "Poppins-SemiBold",
+          color: Colors.green,
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
-      child: const Text("Click"),
+      backgroundColor: Colors.green[800],
+      child: const Text(
+        "Click",
+        style: TextStyle(
+          fontSize: 15.0,
+          fontFamily: "Poppins-SemiBold",
+        ),
+      ),
     ),
   )
 ));
